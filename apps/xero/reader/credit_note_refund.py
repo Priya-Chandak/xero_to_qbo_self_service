8938698f50a4cd9d768077a20faf7ccbb02762ec
Mahesh Gurav <>
2023-10-10 21:32:23 +0530

    Removed account right package

--- apps/xero/reader/credit_note_refund.py
+++ apps/xero/reader/credit_note_refund.py
@@ -1,22 +1,20 @@
-import traceback
+import sys
 
 import requests
 
-from apps.home.data_util import add_job_status
 from apps.home.data_util import get_job_details
+from apps.home.data_util import write_task_execution_step, update_task_execution_status
 from apps.mmc_settings.all_settings import *
 from apps.util.db_mongo import get_mongodb_database
-from apps.home.data_util import  write_task_execution_step,update_task_execution_status
-import sys
 
 
-def get_xero_credit_memo_refund_payment(job_id,task_id):
+def get_xero_credit_memo_refund_payment(job_id, task_id):
     try:
         start_date, end_date = get_job_details(job_id)
         dbname = get_mongodb_database()
         xero_credit_memo_refund_payment = dbname["xero_credit_memo_refund_payment"]
         # xero_invoice_payment = dbname["xero_invoice_payment"]
-        
+
         payload, base_url, headers = get_settings_xero(job_id)
 
         if start_date == "" and end_date == "":
@@ -34,7 +32,7 @@ def get_xero_credit_memo_refund_payment(job_id,task_id):
         if response1.status_code == 200:
             r1 = response1.json()
             r2 = r1["Payments"]
-            if len(r2)>0:
+            if len(r2) > 0:
                 no_of_records = len(r2)
                 no_of_pages = (no_of_records // 100) + 1
 
@@ -61,8 +59,8 @@ def get_xero_credit_memo_refund_payment(job_id,task_id):
 
                     for i in range(0, len(JsonResponse1)):
                         if (
-                            JsonResponse1[i]["Status"] != "DELETED"
-                            and JsonResponse1[i]["Status"] != "VOIDED"
+                                JsonResponse1[i]["Status"] != "DELETED"
+                                and JsonResponse1[i]["Status"] != "VOIDED"
                         ):
                             QuerySet = {}
                             QuerySet["job_id"] = job_id
@@ -103,28 +101,25 @@ def get_xero_credit_memo_refund_payment(job_id,task_id):
                             QuerySet["Contact"] = JsonResponse1[i]["Invoice"]["Contact"]["Name"]
 
                             if JsonResponse1[i]["PaymentType"] == "ARCREDITPAYMENT":
-                                QuerySet["table_name"] = "xero_credit_memo_refund_payment" 
+                                QuerySet["table_name"] = "xero_credit_memo_refund_payment"
                                 credit_memo_refund_payment.append(QuerySet)
                             # elif JsonResponse1[i]["PaymentType"] == "ACCPAYPAYMENT":
                             #     QuerySet["table_name"] = "xero_bill_payment" 
                             #     bill_payment.append(QuerySet)
-                            
 
                 # if len(bill_payment) > 0:
                 #     xero_bill_payment.insert_many(bill_payment)
                 if len(credit_memo_refund_payment) > 0:
                     xero_credit_memo_refund_payment.insert_many(credit_memo_refund_payment)
-             
+
                 step_name = "Reading data from xero credit memo refund"
                 write_task_execution_step(task_id, status=1, step=step_name)
-            
+
     except Exception as ex:
         step_name = "Access token not valid"
         write_task_execution_step(task_id, status=0, step=step_name)
-        update_task_execution_status( task_id, status=0, task_type="read")
+        update_task_execution_status(task_id, status=0, task_type="read")
         import traceback
         traceback.print_exc()
         print(ex)
         sys.exit(0)
-
-        
