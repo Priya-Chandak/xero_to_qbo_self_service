2432c9dedb07c841631d08becadc87f3106c1e59
Priya-Chandak <priya.chandak@vishleshak.io>
2023-10-11 14:51:54 +0530

    Added required API

--- apps/myob/delete_apis.py
+++ apps/myob/delete_apis.py
@@ -5,16 +5,29 @@ def delete_coa(job_id, task_id):
     dbname = get_mongodb_database()
     dbname["QBO_COA"].delete_many({'job_id': job_id})
 
-
 def delete_item(job_id, task_id):
     dbname = get_mongodb_database()
     dbname["QBO_Item"].delete_many({'job_id': job_id})
 
+def delete_invoice(job_id, task_id):
+    dbname = get_mongodb_database()
+    dbname["QBO_Invoice"].delete_many({'job_id': job_id})
+def delete_creditnote(job_id, task_id):
+    dbname = get_mongodb_database()
+    dbname["QBO_CreditNote"].delete_many({'job_id': job_id})
+def delete_bill(job_id, task_id):
+    dbname = get_mongodb_database()
+    dbname["QBO_Bill"].delete_many({'job_id': job_id})
+def delete_customer(job_id, task_id):
+    dbname = get_mongodb_database()
+    dbname["QBO_Customer"].delete_many({'job_id': job_id})
+def delete_supplier(job_id, task_id):
+    dbname = get_mongodb_database()
+    dbname["QBO_Supplier"].delete_many({'job_id': job_id})
 
 def delete_chart_of_account(job_id, task_id):
     dbname = get_mongodb_database()
     dbname["chart_of_account"].drop()
-
 
 def delete_excel_reckon_invoice(job_id, task_id):
     dbname = get_mongodb_database()
